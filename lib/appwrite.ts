56133ee94b3d0a34aeb987ad7e84b77c111ab9fc
Gustavo Torres <betogus2009@gmail.com>
2025-08-15 01:22:53 +0000

    adding auth view

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appWriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.android.puntoysabor",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "689e6bdb003860912805",
+    userCollectionId: "689e6c7b0033221c1ec4"
+}
